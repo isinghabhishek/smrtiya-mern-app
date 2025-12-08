91347f5187424d83f2dbe4a3a6af700f3a598ca7
isinghabhishek <singhabhishek3006.abhi@gmail.com>
2025-12-09 00:38:35 +0530

    Remove server gitlink/submodule placeholder and clean up metadata

---
+++ client/build/precache-manifest.c7162c38ec246069f7d2edae27a6ff9b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08e29d2d00ac3c018a0a9e6c0862dd3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65439a6676e6fb34d1e7",
+    "url": "/static/css/main.9516a8fc.chunk.css"
+  },
+  {
+    "revision": "de6221837bc0e6dc8a37",
+    "url": "/static/js/2.9d8202a9.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.9d8202a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65439a6676e6fb34d1e7",
+    "url": "/static/js/main.0f8e2bb0.chunk.js"
+  },
+  {
+    "revision": "48ad488e64bdc404f9e2",
+    "url": "/static/js/runtime-main.7ad89733.js"
+  },
+  {
+    "revision": "dc8bbfe30b9b1cf8867a83e69d051651",
+    "url": "/static/media/memoriesLogo.dc8bbfe3.png"
+  },
+  {
+    "revision": "17d51a9b845dc11524a86eb33cd0e645",
+    "url": "/static/media/smrtiyanText.17d51a9b.png"
+  }
+]);
