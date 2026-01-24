06545428e163146aacd452a6385d3f42a3834135
isinghabhishek <singhabhishek3006.abhi@gmail.com>
2026-01-24 22:48:32 +0530

    added: comment in models

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -1,19 +1,29 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
+// Define the schema for a post message document
 const postSchema = mongoose.Schema({
-    title: String,
-    message: String,
-    name: String,
-    creator: String,
-    tags: [String],
-    selectedFile: String,
-    likes: { type: [String], default: [] },
-    createdAt: {
-        type: Date,
-        default: new Date(),
-    },
-})
+  // Title of the post
+  title: String,
+  // Main content/body of the post
+  message: String,
+  // Name of the person who created the post
+  name: String,
+  // User ID of the creator
+  creator: String,
+  // Array of tags associated with the post
+  tags: [String],
+  // URL or file path of the selected image
+  selectedFile: String,
+  // Array of user IDs who have liked this post
+  likes: { type: [String], default: [] },
+  // Timestamp when the post was created
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
 
-var PostMessage = mongoose.model('PostMessage', postSchema);
+// Create and export the PostMessage model based on the schema
+var PostMessage = mongoose.model("PostMessage", postSchema);
 
 export default PostMessage;
--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
 
+// Define the schema for a user document
 const userSchema = mongoose.Schema({
-  name: { type: String, required:  true },
+  // User's full name (required field)
+  name: { type: String, required: true },
+  // User's email address (required field)
   email: { type: String, required: true },
+  // User's password (required field)
   password: { type: String, required: true },
+  // User's unique ID
   id: { type: String },
 });
 
+// Create and export the User model based on the schema
 export default mongoose.model("User", userSchema);
